79fd9371926e84da79743ab0423037a0f73a0d3c
Gongada Mukesh <mukeshgongada10@gmail.com>
2023-01-24 15:11:09 +0530

    done

--- src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
+++ src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
@@ -1,6 +1,6 @@
 package com.epam.rd.autotasks.arrays;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class LocalMaximaRemove {
 
@@ -13,7 +13,12 @@ public class LocalMaximaRemove {
     public static int[] removeLocalMaxima(int[] array){
 
         //put your code here
-
-        throw new UnsupportedOperationException();
+        List<Integer> l = new ArrayList<Integer>();
+        for(int i=0;i<array.length;i++){
+            if(i>0 && array[i]<=array[i-1] || (i != array.length-1 && array[i]<=array[i+1])){
+                l.add(array[i]);
+            }
+        }
+        return l.stream().mapToInt(i->i).toArray();
     }
 }
